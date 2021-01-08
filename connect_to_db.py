17e8329abffbe69db980b7e38a386402c6462cc6
Rafal Jakubczyk <jakubczykrafal06@gmail.com>
2021-01-08 13:19:52 +0100

    aaa

--- connect_to_db.py
+++ connect_to_db.py
@@ -1,9 +1,10 @@
 import os
 import urllib.parse as up
 import psycopg2
+from connect_to_db import db_url
 
 up.uses_netloc.append("postgres")
-url = up.urlparse("postgres://hteoajtw:2uSjqGQVjyBFqIFZVX2u_mJndOIN9kpk@dumbo.db.elephantsql.com:5432/hteoajtw")
+url = up.urlparse(db_url)
 conn = psycopg2.connect(database=url.path[1:],
     user=url.username,
     password=url.password,
